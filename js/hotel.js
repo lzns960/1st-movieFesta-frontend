7c7ce8499d1e9b46cd4b1250bdcac4daa06092a2
lzns960 <lzns960@gmail.com>
2022-08-10 00:35:34 +0900

    update barbie & hotel

--- js/hotel.js
+++ js/hotel.js
@@ -1,3 +1,77 @@
+/* 스크롤 애니메이션 */
+(() => {
+    let yOffset = 0; //window.pageYOffset 대신 쓸 변수 
+    let prevScrollheight = 0; //현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 아티클 높이 값의 합
+    let currentScene = 0; //현재 활성화된 씬 (scrollArticle)
+    const sceneInfo = [
+        {
+            //0
+            type: 'sticky',
+            heightNum: 5, //브라우저 높이의 5배로 scrollHeight를 세팅
+            scrollHeight:0,
+            objs: {
+                container: document.querySelector('.scrollArticle0')
+            }
+        },
+
+        {
+            //1
+            type: 'normal',
+            heightNum: 5, //브라우저 높이의 5배로 scrollHeight를 세팅
+            scrollHeight:0,
+            objs: {
+                container: document.querySelector('.scrollArticle1')
+            }
+        },
+    ];
+
+    function setLayout(){
+        //각 스크롤 아티클의 높이 세팅
+        for (let i = 0; i < sceneInfo.length; i++) {
+            sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+        }
+
+        yOffset = window.pageYOffset;
+        let totalScrollHeight = 0;
+        for (let i = 0; i < sceneInfo.length; i++){
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+
+            if (totalScrollHeight >= YOffset){
+                currentScene = i;
+                break;
+            }
+            document.body.setAttribute('id', `showScene${currentScene}`)
+        }
+    }
+
+    function scrollLoop(){
+        prevScrollheight = 0; 
+        for (let i = 0; i < currentScene; i++) {
+            prevScrollheight += sceneInfo[i].scrollHeight;
+        }
+
+        if (yOffset > prevScrollheight +sceneInfo[currentScene].scrollHeight) {
+            currentScene++;
+            document.body.setAttribute('id', `showScene${currentScene}`);
+        }
+        if (yOffset < prevScrollheight) {
+            if (currentScene === 0 ) return; //브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지
+            currentScene--;
+            document.body.setAttribute('id', `showScene${currentScene}`);
+        }
+    }
+
+    window.addEventListener('resize', setLayout);
+    window.addEventListener('scroll', ()=> {
+        yOffset = window.pageYOffset;
+        scrollLoop();
+    });
+
+    window.addEventListener('load', setLayout);
+    window.addEventListener('resize', setLayout);
+}) ();
+
 //마퀴 텍스트 효과
 const lineText1 = document.querySelector('.lineText1')
 const lineText2 = document.querySelector('.lineText2')
@@ -64,3 +138,4 @@ AOS.init({
     duration: 1000,
 });
 */
+
